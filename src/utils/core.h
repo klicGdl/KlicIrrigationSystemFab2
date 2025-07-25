2c99c4bf028c4abf8cf6a55e42e770a5a2fade5f
Daniel Polanco <jdanypa@gmail.com>
2025-07-25 15:17:07 -0600

    [WiFi Driver] Initial Implementation (#13)
    Signed-off-by: Daniel Polanco <jdanypa@gmail.com>

---
+++ src/utils/core.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Project: Klic Smart Irrigation System
+ *
+ * Description:
+ * Core utils library to hold any non-specifics utils that can be shared
+ * in other utils/drivers.
+ *
+ * License:
+ *    This project is open for use, modification, and distribution for personal
+ *    or educational purposes only. Commercial use is strictly prohibited
+ *    without explicit permission from the author
+ ******************************************************************************/
+#pragma once
+
+#define ARRAY_SIZE(__arr) (sizeof(__arr) / sizeof(__arr[0]))
---
+++ src/wifi/WiFiDriver.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+ * Project: Klic Smart Irrigation System
+ *
+ * Description:
+ * Library intended to initialize the WiFi drivers for an Arduino-based ESP32
+ * CPU
+ *
+ * License:
+ *    This project is open for use, modification, and distribution for personal
+ *    or educational purposes only. Commercial use is strictly prohibited
+ *    without explicit permission from the author
+ ******************************************************************************/
+#include "WiFiDriver.h"
+
+#include "utils/logger.h"
+
+void _configureWiFiManager() {
+  WiFiDriver::_wm.setConnectTimeout(120);
+  WiFiDriver::_wm.setConfigPortalTimeout(180);
+}
+
+namespace WiFiDriver {
+
+// Namespace extern definitions
+namespace Config {
+const char* WIFI_SSID = "KLIC-ISFAB2";
+const char* WIFI_PASS = "ROOT";
+}  // namespace Config
+
+WiFiManager _wm;
+
+// Exposed methods
+
+bool WiFiInitialize(void) {
+  WiFi.mode(WIFI_STA);
+  _configureWiFiManager();
+
+  // will try to connect when it has saved credentials
+  // if it fails will start the access point
+  bool connected = WiFiDriver::_wm.autoConnect(WiFiDriver::Config::WIFI_SSID,
+                                               WiFiDriver::Config::WIFI_PASS);
+  if (!connected) {
+    logger << LOG_ERROR << "WiFi AutoConnect failed to: "
+           << WiFiDriver::_wm.getWiFiSSID(false)
+           << " with pass: " << WiFiDriver::_wm.getWiFiPass(false) << EndLine;
+    return false;
+  }
+
+  logger << LOG_INFO << "WiFi AutoConnect done! IP: " << WiFi.localIP()
+         << EndLine;
+
+  return true;
+}
+
+}  // namespace WiFiDriver
+
